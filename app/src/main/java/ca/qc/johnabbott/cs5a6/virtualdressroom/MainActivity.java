bdde43ebe2d4b5ecfea0682d3fec4189c35fab49
danhalis <danhalisly@gmail.com>
2021-11-24 18:45:49 -0800

    initialize project

---
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.qc.johnabbott.cs5a6.virtualdressroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
