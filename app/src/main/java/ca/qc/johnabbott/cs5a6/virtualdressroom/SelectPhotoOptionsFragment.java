b7f17f610308d350372a1ec89111ad2e3c3b8385
gjj08519 <gjj085199@gmail.com>
2021-12-01 21:53:09 -0500

    add

--- app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/SelectPhotoOptionsFragment.java
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/SelectPhotoOptionsFragment.java
@@ -1,64 +1,75 @@
 package ca.qc.johnabbott.cs5a6.virtualdressroom;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SelectPhotoOptionsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import ca.qc.johnabbott.cs5a6.virtualdressroom.databinding.FragmentSelectPhotoOptionsBinding;
+
+
 public class SelectPhotoOptionsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private FragmentSelectPhotoOptionsBinding binding;
+    private MainActivity mainActivity;
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
     public SelectPhotoOptionsFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SelectPhotoOptionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SelectPhotoOptionsFragment newInstance(String param1, String param2) {
-        SelectPhotoOptionsFragment fragment = new SelectPhotoOptionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_photo_options, container, false);
+        binding = FragmentSelectPhotoOptionsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.SelectPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NavController controller = Navigation.findNavController(mainActivity, R.id.nav_host_fragment_content_main);
+                controller.navigate(R.id.action_selectPhotoOptionsFragment_to_selectPhotoFragment);
+            }
+        });
+        binding.CameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(mainActivity, R.id.nav_host_fragment_content_main);
+                controller.navigate(R.id.action_selectPhotoOptionsFragment_to_cameraFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+
     }
 }
