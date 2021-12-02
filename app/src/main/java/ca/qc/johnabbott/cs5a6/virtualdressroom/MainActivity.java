17ad4e890d1c8eac7f483754649882d4cbd0ef24
Hieu Dao Le Duc <danhalisly@gmail.com>
2021-12-01 23:09:56 -0800

    reduce CropImage to one fragment

--- app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/MainActivity.java
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/MainActivity.java
@@ -2,19 +2,28 @@ package ca.qc.johnabbott.cs5a6.virtualdressroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import ca.qc.johnabbott.cs5a6.virtualdressroom.ui.editor.EditPhotoActivity;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.ui.editor.CropPhotoFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CropPhotoFragment cropPhotoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, EditPhotoActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, EditPhotoActivity.class);
+//        startActivity(intent);
+    }
+
+    public void setCropPhotoFragment(CropPhotoFragment cropPhotoFragment) {
+        this.cropPhotoFragment = cropPhotoFragment;
+    }
+
+    public CropPhotoFragment getCropPhotoFragment() {
+        return cropPhotoFragment;
     }
 }
