8df9615b77cd683ff09cb1e3af0232096035ae45
Hieu Dao Le Duc <danhalisly@gmail.com>
2021-12-01 18:53:01 -0800

    add CropView to main project

--- app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/model/ClothingStatus.java
+++
@@ -1,7 +0,0 @@
-package ca.qc.johnabbott.cs5a6.virtualdressroom.model;
-
-public enum ClothingStatus
-{
-    AVAILABLE,
-    DELETED
-}
---
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/ui/models/Point.java
@@ -0,0 +1,11 @@
+package ca.qc.johnabbott.cs5a6.virtualdressroom.ui.models;
+
+public class Point {
+
+    public float x, y;
+
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
+}
