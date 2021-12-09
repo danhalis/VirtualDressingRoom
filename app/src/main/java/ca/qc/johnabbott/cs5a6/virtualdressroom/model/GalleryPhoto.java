f1cb37895d90c3d6beb8c3dac3b4259f40c61bde
gjj08519 <gjj085199@gmail.com>
2021-12-09 13:19:17 -0500

    add basic database for test gallery

---
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/model/GalleryPhoto.java
@@ -0,0 +1,54 @@
+package ca.qc.johnabbott.cs5a6.virtualdressroom.model;
+
+import android.content.Context;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.sqlite.Identifiable;
+
+
+
+
+
+public class GalleryPhoto implements Identifiable<Long> {
+    private String webformatURL;
+    private long id;
+
+
+    public GalleryPhoto(String webformatURL, long id, String largeImageURL) {
+        this.webformatURL = webformatURL;
+        this.id = id;
+
+    }
+
+    public GalleryPhoto() {
+
+    }
+
+    public GalleryPhoto(Context context) {
+    }
+
+    public String getWebformatURL() {
+        return webformatURL;
+    }
+
+    public GalleryPhoto setWebformatURL(String webformatURL) {
+        this.webformatURL = webformatURL;
+        return this;
+    }
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Identifiable<Long> setId(Long id) {
+        return this;
+    }
+    public GalleryPhoto setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+
+}
---
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/model/GalleryPhotoDBHandler.java
@@ -0,0 +1,80 @@
+package ca.qc.johnabbott.cs5a6.virtualdressroom.model;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+import ca.qc.johnabbott.cs5a6.virtualdressroom.model.GalleryPhoto;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.model.GalleryPhotoTable;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.sqlite.DatabaseException;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.sqlite.Table;
+
+import java.sql.Date;
+import java.text.ParseException;
+
+import ca.qc.johnabbott.cs5a6.virtualdressroom.sqlite.DatabaseException;
+import ca.qc.johnabbott.cs5a6.virtualdressroom.sqlite.Table;
+
+
+public class GalleryPhotoDBHandler extends SQLiteOpenHelper {
+
+    public static final String DATABASE_FILE_NAME = "110galleryPhotos.db";
+    public static final int DATABASE_VERSION = 1;
+    public enum State { NONE, BEFORE_CHOOSE,CHOOSED,BEFORE_EDIT, EDITED, BEFORE_CREATE, CREATED}
+
+    private GalleryPhotoDBHandler.State state;
+    private GalleryPhoto originalPhoto;
+    private GalleryPhoto updatedPhoto;
+
+    private Table<GalleryPhoto> galleryPhotoTable;
+
+    public GalleryPhotoDBHandler(@Nullable Context context) {
+        super(context, DATABASE_FILE_NAME, null, DATABASE_VERSION);
+        galleryPhotoTable = new GalleryPhotoTable(this);
+    }
+    public Table<GalleryPhoto> getGalleryPhotoTable() {
+        return galleryPhotoTable;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            galleryPhotoTable.createTable(db);
+
+        } catch (ParseException | DatabaseException e) {
+            e.printStackTrace();
+        }
+        if( galleryPhotoTable.hasInitialData()) {
+            try {
+                galleryPhotoTable.initialize(db);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            } catch (DatabaseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        galleryPhotoTable.dropTable(db);
+        onCreate(db);
+    }
+    public void setState(GalleryPhotoDBHandler.State state) {
+        this.state = state;
+    }
+    public GalleryPhoto getOriginalPhoto() {
+        return originalPhoto;
+    }
+    public void setOriginalPhoto(GalleryPhoto originalPhoto) {
+        this.originalPhoto = originalPhoto;
+    }
+
+
+}
+
+
