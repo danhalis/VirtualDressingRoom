4a88792925bb6a96304c8ad80bd64128a185b221
NickNitt1735 <nnittolo1309abbott@gmail.com>
2021-12-15 20:16:37 -0500

    Load Into Recycler Views
    The edited image will now be loaded into the recycler view

--- app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/ui/list/ModelFragment.java
+++ app/src/main/java/ca/qc/johnabbott/cs5a6/virtualdressroom/ui/list/ModelFragment.java
@@ -101,6 +101,8 @@ public class ModelFragment extends Fragment
 
                 adapter.setClothingItems(clothingItems);
 
+                activity.getCropPhotoViewModel().reset();
+
                 adapter.notifyDataSetChanged();
             }
 
